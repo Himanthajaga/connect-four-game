bdff30efbeeaa0c11b5f195727d8d86f58c26159
J.G Himantha <himanthagamachchige@gmail.com>
2024-03-13 12:26:01 +0530

    first commit

--- src/main/java/lk/ijse/dep/service/BoardUI.java
+++ src/main/java/lk/ijse/dep/service/BoardUI.java
@@ -1,11 +1,2 @@
-package lk.ijse.dep.service;
-
-public class BoardUI {
-    public void update(int col, boolean isHuman) {
-
-    }
-
-    public void notifyWinner(Winner winner) {
-
-    }
+package lk.ijse.dep.service;public interface BoardUI {
 }
